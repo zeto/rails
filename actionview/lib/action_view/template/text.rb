ffbbcd84f04e4996ff2e838995254634a5d30ddd
Piotr Sarnacki <drogus@gmail.com>
2012-06-15 17:17:21 +0200

    Remove Mime::Type translations from Action View
    Action View should not be responsible for translating mime types. Any
    formats that are passed to Action View classes should be in form of
    symbols, like :xml or :json. Any translation that's needed should be
    handled at controller level.

--- actionview/lib/action_view/template/text.rb
+++ actionview/lib/action_view/template/text.rb
@@ -2,12 +2,11 @@ module ActionView #:nodoc:
   # = Action View Text Template
   class Template
     class Text < String #:nodoc:
-      attr_accessor :mime_type
+      attr_accessor :type
 
-      def initialize(string, mime_type = nil)
+      def initialize(string, type = nil)
         super(string.to_s)
-        @mime_type   = Mime[mime_type] || mime_type if mime_type
-        @mime_type ||= Mime::TEXT
+        @type = type || :text
       end
 
       def identifier
@@ -23,7 +22,7 @@ module ActionView #:nodoc:
       end
 
       def formats
-        [@mime_type.to_sym]
+        [@type.to_sym]
       end
     end
   end
